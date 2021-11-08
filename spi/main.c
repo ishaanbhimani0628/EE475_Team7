2d6bfd1218e7465a85308f19b29ed8f04ad0e8f7
ishaanbhimani0628 <ishaanb@uw.edu>
2021-11-08 12:42:44 -0800

    change SPI CPOL and CPHA to 0, successfully reading X axis, but not other axes

--- spi/main.c
+++ spi/main.c
@@ -12,10 +12,15 @@ uint16_t SpiReadAccel(uint16_t cmd);
 void ADXL362_begin();
 uint16_t readXData();
 void ADXL362_beginMeasure();
+void readXYZData(int16_t* xdata, int16_t* ydata, int16_t* zdata, int16_t* temp);
 
 
 
 volatile uint16_t read_data = 0;
+volatile int16_t x_data = 0;
+volatile int16_t y_data = 0;
+volatile int16_t z_data = 0;
+volatile int16_t t_data = 0;
 
 int main(void)
 {
@@ -24,7 +29,7 @@ int main(void)
 	ADXL362_begin();
 	ADXL362_beginMeasure();
 	while(1) {
-	    read_data = readXData();
+	    readXYZData(&x_data, &y_data, &z_data, &t_data);
 	}
 }
 
@@ -36,6 +41,8 @@ void ConfigSPI( void )
   USICTL0   = USISWRST;                                    // USI in reset state
   USICTL0  |= (USIPE7 | USIPE6 | USIPE5 | USIMST | USIOE); // SDI, SDO and CLK enabled, MSB first, master mode, output enabled
   USICKCTL  = (USIDIV_4 | USISSEL_2);                      // Clock divider 16, SMCLK, clock idles low (changed on 1st, captured on 2nd edge)
+  USICKCTL &= ~(USICKPL); // set CPOL to 0 to match ADXL362
+  USICTL1 |= USICKPH; //change CPHA to 0 to match ADXL362
   USICTL0  &= ~USISWRST;                                   // USI released from reset
 }
 
@@ -105,6 +112,60 @@ uint16_t readXData() {
     uint16_t xdata = USISR;
     P1OUT |= BIT4;
     return xdata;
+}
+
+void readXYZData(int16_t* xdata, int16_t* ydata, int16_t* zdata, int16_t* temp) {
+    P1OUT &= ~(BIT4); // set CS low
+
+    // read x data
+    USISR = 0x0B;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x0E;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x0;
+    USICNT = (USI16B | 16);
+    while( !(USICTL1 & USIIFG) );
+    *xdata = USISR;
+
+    // read y= data
+    USISR = 0x0B;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x10;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x0;
+    USICNT = (USI16B | 16);
+    while( !(USICTL1 & USIIFG) );
+    *ydata = USISR;
+
+    // read z data
+    USISR = 0x0B;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x12;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x0;
+    USICNT = (USI16B | 16);
+    while( !(USICTL1 & USIIFG) );
+    *zdata = USISR;
+
+    // read y data
+    USISR = 0x0B;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x14;
+    USICNT = 8;
+    while( !(USICTL1 & USIIFG) );
+    USISR = 0x0;
+    USICNT = (USI16B | 16);
+    while( !(USICTL1 & USIIFG) );
+    *temp = USISR;
+
+    P1OUT |= BIT4;
 }
 
 
